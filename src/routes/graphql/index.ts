1ab71cefcace2056a63acca6f9887fa5e33822f9
massaracsh7 <fatum7@gmail.com>
2024-02-28 21:16:43 +0300

    feat: add member type

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,9 +1,47 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { GraphQLSchema, graphql } from 'graphql';
+import { GraphQLBoolean, GraphQLEnumType, GraphQLFloat, GraphQLInt, GraphQLList, GraphQLNonNull, GraphQLObjectType, GraphQLSchema, GraphQLString, graphql } from 'graphql';
+import { PrismaClient } from '@prisma/client';
+
+enum MemberTypeId {
+  BASIC = 'basic',
+  BUSINESS = 'business',
+}
+
+export const memberType = new GraphQLObjectType({
+  name: 'Member',
+  fields: () => ({
+    id: { type: new GraphQLNonNull(memberTypeIdEnum) },
+    discount: { type: GraphQLFloat },
+    postsLimitPerMonth: { type: GraphQLInt },
+  }),
+});
+
+
+const queryType = new GraphQLObjectType({
+  name: 'Query',
+  fields: {
+    memberTypes: {
+      type: new GraphQLList(memberType),
+      async resolve(parent, args, context: { prisma: PrismaClient }) {
+        return await context.prisma.memberType.findMany();
+      },
+    },
+
+  },
+});
+
+const memberTypeIdEnum = new GraphQLEnumType({
+  name: 'MemberTypeId',
+  values: {
+    [MemberTypeId.BASIC]: { value: MemberTypeId.BASIC },
+    [MemberTypeId.BUSINESS]: { value: MemberTypeId.BUSINESS },
+  },
+});
+
 
 const schema = new GraphQLSchema({
-  
+  query: queryType,
 });
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
