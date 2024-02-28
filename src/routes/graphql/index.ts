f1541048b7567554a6921ccf11c782e1ae318217
massaracsh7 <fatum7@gmail.com>
2024-02-28 18:56:04 +0300

    init: start task

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,8 +1,14 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { GraphQLSchema, graphql } from 'graphql';
+
+const schema = new GraphQLSchema({
+  
+});
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
+  const { prisma } = fastify;
+
   fastify.route({
     url: '/',
     method: 'POST',
@@ -13,7 +19,14 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
-      return {};
+      const { query, variables } = req.body;
+      const result = await graphql({
+        schema,
+        source: query,
+        contextValue: { prisma },
+        variableValues: variables,
+      });
+      return result;
     },
   });
 };
